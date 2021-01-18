19ae384918303b4f7b833653c65a18d344fad2b6
Jenhao <jenhao.hsiao@gmail.com>
2021-01-17 22:49:02 -0500

    Basic Level Design

--- Assets/Rocket.cs
+++ Assets/Rocket.cs
@@ -6,27 +6,45 @@ using UnityEngine;
 public class Rocket : MonoBehaviour
 {
 
+    [SerializeField] float rcsThrust = 100f;
+    [SerializeField] float mainThrust = 100f;
     Rigidbody rigidbody;
     AudioSource m_MyAudioSource;
+
 
     void Start()
     {
         rigidbody = GetComponent<Rigidbody>();
         m_MyAudioSource = GetComponent<AudioSource>();
+        rigidbody.mass = 0.08f;
 
     }
 
     // Update is called once per frame
     void Update()
     {
-        ProcessInput();
+        Rotate();
+        Thrust();
     }
 
-    void ProcessInput()
+    void OnCollisionEnter(Collision collision)
     {
+        switch (collision.gameObject.tag)
+        {
+            case "Friendly":
+                print("Friendly");
+                break;
+            case "ok":
+                print("OK");
+                break;
+            case "dead":
+                print("Dead");
+                break;
+        }
+    }
 
-
-
+    private void Thrust()
+    {
         if (Input.GetKey(KeyCode.Space)) // can thrust while rotating
         {
             rigidbody.AddRelativeForce(Vector3.up);
@@ -41,20 +59,31 @@ public class Rocket : MonoBehaviour
         {
             m_MyAudioSource.Stop();
         }
+    }
+
+    void Rotate()
+    {
+
+        rigidbody.freezeRotation = true;
+        float rotationThisFrame = rcsThrust * Time.deltaTime;
 
         if (Input.GetKey(KeyCode.A)) // can thrust while rotating
         {
-            //rigidbody.AddRelativeForce(Vector3.right);
-            transform.Rotate(Vector3.forward);
-             
+            transform.Rotate(Vector3.forward * rotationThisFrame);
+
         }
 
         if (Input.GetKey(KeyCode.D)) // can thrust while rotating
         {
-            //rigidbody.AddRelativeForce(Vector3.left);
-            transform.Rotate(-Vector3.forward);
-           
+            transform.Rotate(-Vector3.forward * rotationThisFrame);
+
         }
 
+        rigidbody.freezeRotation = false;
+
     }
+
+
+
+
 }
